44b020415a333582ce6c367fb276ff6f35500605
Ayashi <ayashi@valefor.com>
2015-04-18 23:38:35 -0700

    working on prototypes for the category, home, and video pages

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Plugins
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+
+// compile less
+gulp.task('less', function () {
+  return gulp.src('./*.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('.'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('*.less', ['less']);
+});
+
+// default task
+gulp.task('default', ['less', 'watch']);
